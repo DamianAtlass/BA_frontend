f1b70ccfe1963d90efd57a28c2a040da28d99cec
DamianAtlass <damianatlass@gmx.de>
2023-01-02 23:06:09 +0100

    first working modal

--- react-frontend/src/components/CreateUserModal.js
+++ react-frontend/src/components/CreateUserModal.js
@@ -1,21 +1,81 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import axios from "axios";
+import { API_URL } from "../constants";
 
-export default function CreateUserModal(){
+export default function CreateUserModal() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const email = useRef()
+    const password = useRef()
+    const username = useRef()
+
+    const what = "WHAT"
+
+    function foo(){
+        const data = {
+            "email": email.current.value,
+            "password": password.current.value,
+            "username": username.current.value
+        }
+        axios.post(API_URL+"createuser/", data)
+    }
+
     return (
-        <div className="moddal show">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h4 className="modal-title"> Modal-Title  </h4>
-                </div>
-                <div className="modal-body">
-                    this is the body
-                </div>
-                <div className="modal-footer">
-                    <button className="button">Close</button>
-                </div>
-            </div>
-            Hello Worldsadfsdf
-            <input type="text"/>
-        </div>
-    )
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Launch static backdrop modal
+            </Button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Create user</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {/*info here*/}
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control ref={email}
+                                type="email"
+                                placeholder="name@example.com"
+                                autoFocus
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control ref={username}
+                                          type="text"
+                                          placeholder="username"
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control ref={password}
+                                          type="password"
+                                          placeholder="***********"
+                            />
+                        </Form.Group>
+
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={foo}>Understood</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
--- react-frontend/src/components/WelcomePage.js
+++ react-frontend/src/components/WelcomePage.js
@@ -1,14 +1,13 @@
 import React from "react"
 import CreateUserModal from "./CreateUserModal";
 import TestComponent from "./TestComponent";
-import NewModal from "./NewModal";
 
 
 export default function WelcomePage(){
     return (
         <>
             <p>Welcome</p>
-            <NewModal/>
+            <CreateUserModal/>
         </>
     )
 }
