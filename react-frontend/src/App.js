403863c917be59a6458ca704ec7b15f102b7c817
damianwastaken <damianatlass@gmx.de>
2023-01-25 02:42:41 +0100

    added react context

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -7,24 +7,27 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {BrowserRouter, Routes, Route, useNavigate, Navigate} from "react-router-dom";
 import OverviewPage from "./components/OverviewPage";
+import {UserContext} from "./components/contexts/UserDataContext";
 function App() {
   return(
 
-      <Container fluid className="MainContainer">
-          <Row className="justify-content-center" >
-              <Col sx={12} sm={8} style={{"border-style": "dotted"}}>
-                  <BrowserRouter>
-                      <Routes>
-                          <Route path="welcome" element={ <WelcomePage/> }/>
-                          <Route path="overview" element={ <OverviewPage/> }/>
-                          <Route index element={ <Navigate to={"/welcome"}/> }/>
-                          <Route path="*"  element={<div> oh no, you're lost! 404 </div>} />
-                      </Routes>
-                  </BrowserRouter>
-              </Col>
-          </Row>
+      <UserContext>
+          <Container fluid className="MainContainer">
+              <Row className="justify-content-center" >
+                  <Col sx={12} sm={8} style={{"border-style": "dotted"}}>
+                      <BrowserRouter>
+                          <Routes>
+                              <Route path="welcome" element={ <WelcomePage/> }/>
+                              <Route path="overview" element={ <OverviewPage/> }/>
+                              <Route index element={ <Navigate to={"/welcome"}/> }/>
+                              <Route path="*"  element={<div> oh no, you're lost! 404 </div>} />
+                          </Routes>
+                      </BrowserRouter>
+                  </Col>
+              </Row>
 
-      </Container>
+          </Container>
+      </UserContext>
 
 
 
---
+++ react-frontend/src/components/contexts/UserDataContext.js
@@ -0,0 +1,41 @@
+import React, {useState, useContext, useRef} from "react";
+
+const UserDataContext = React.createContext()
+const UserDataUpdateContext = React.createContext()
+
+export function useUserData(){
+    return useContext(UserDataContext)
+}
+export function useUserDataUpdate(){
+    return useContext(UserDataUpdateContext)
+}
+
+export function UserContext({children}){
+    const default_state = {
+        "username": null
+    }
+    const [userData, setUserData] = useState(default_state)
+
+    function changeUserData(newUserData){ return ()=>{
+        for (const [key, value] of Object.entries(newUserData)) {
+            if (userData.hasOwnProperty(key)){
+                const updated_state = {...userData}
+                updated_state[key] = value
+                setUserData(updated_state)
+            } else {
+                console.log("No prop", key, "!")
+            }
+        }
+    }
+
+    }
+
+
+    return(
+        <UserDataContext.Provider value={userData}>
+            <UserDataUpdateContext.Provider value={changeUserData}>
+                {children}
+            </UserDataUpdateContext.Provider>
+        </UserDataContext.Provider>
+    )
+}
