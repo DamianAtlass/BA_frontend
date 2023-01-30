283fbd0698ae8ab457368bca48b21b7d5586c4ff
damianwastaken <damianatlass@gmx.de>
2023-01-30 21:24:46 +0100

    handled small error

--- react-frontend/src/components/WelcomePage.js
+++ react-frontend/src/components/WelcomePage.js
@@ -12,7 +12,7 @@ export default function WelcomePage(){
     return (
         <Card>
             <Container fluid className="WelcomePage" >
-                <Row flex className="Row justify-content-center">
+                <Row flex="flex" className="Row justify-content-center">
                     <Col className="justify-content-center" align="center">
                         <h1>Welcome</h1>
                     </Col>
