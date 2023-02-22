a658b8d3675f1af2c92bb254e6f23bb90e609ef5
damianwastaken <damianatlass@gmx.de>
2023-02-22 23:50:33 +0100

    wip present custom link in CompleteComponent

--- react-frontend/src/components/SurveyComponent.js
+++ react-frontend/src/components/SurveyComponent.js
@@ -1,17 +1,21 @@
 import 'survey-core/defaultV2.min.css';
 import {Model} from 'survey-core';
 import {Survey} from 'survey-react-ui';
-import {useCallback} from 'react';
+import {useCallback, useState} from 'react';
 import {useUserData} from "./contexts/UserDataContext";
 import { API_URL } from "../constants";
 import axios from "axios";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import CreateUserModal from "./CreateUserModal";
+import LoginModal from "./LoginModal";
 
 const surveyJson = {
     title: "Give your opinion!",
-
     firstPageIsStarted: true,
     startSurveyText: "Start Quiz",
-    completedHtml: "<h4>Thank you very much for taking part in this study!</h4>",
     pages: [{
         elements: [{
             name: "nps-score1",
@@ -24,10 +28,43 @@ const surveyJson = {
     }]
 };
 
+function CompleteCard(){
+    const userData = useUserData(false)
+    let user_pk = userData.user_pk
+    const user_pk_str_pad = user_pk.toString().padStart(3, '0')
+
+    const url = window.location.href
+    console.log("currentUrl:",url)
+    //slice last word
+    const customLink = `${url.slice(0, url.length-6)}login?invitedby=${user_pk_str_pad}`
+
+    console.log("customLink: ", customLink)
+
+
+    return (
+        <Card>
+            <Container fluid>
+                <Row className="justify-content-center">
+                    <Col className="justify-content-center" align="center">
+                        <h1>Thx for your partaking</h1>
+                    </Col>
+                </Row>
+                <Row className="justify-content-evenly">
+                    <Col align="center">
+                        {customLink}
+                    </Col>
+                </Row>
+            </Container>
+        </Card>
+    )
+}
+
 
 export default function SurveyComponent() {
 
-    const userData = useUserData()
+    const [complete, setComplete] = useState(false)
+
+    const userData = useUserData(false)
     let user_pk = userData.user_pk
     const user_pk_str_pad = user_pk.toString().padStart(3, '0')
 
@@ -46,10 +83,12 @@ export default function SurveyComponent() {
             API_URL + "surveydata/" + user_pk_str_pad+"/",
             sender.data
         )
+        setComplete(true)
     }, []);
 
     survey.onComplete.add(surveyComplete);
 
-    return <Survey model={survey}/>;
+
+    return complete ? <CompleteCard/> : <Survey model={survey}/>;
 }
 
