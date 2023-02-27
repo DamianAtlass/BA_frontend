27b1eccd7b2c27a55760c024a6218fb7c2d6c05d
damianwastaken <damianatlass@gmx.de>
2023-02-27 14:48:21 +0100

    working input validation (no rules yet)

--- react-frontend/src/components/CreateUserModal.js
+++ react-frontend/src/components/CreateUserModal.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button"
 import Form from 'react-bootstrap/Form';
 import axios from "axios";
 import { API_URL } from "../constants";
@@ -8,12 +8,11 @@ import { API_URL } from "../constants";
 export default function CreateUserModal() {
     const [show, setShow] = useState(false);
     const [infoMessage, setInfoMessage] = useState("")
-
     const [invitedBy, setInvitedBy] = useState(null)
 
-    let username = useRef("")
-    let email = useRef("")
-    let password= useRef("")
+    const [ formState, setFormState ] = useState({})
+    const [ errorsState, setErrorsState ] = useState({})
+
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -41,29 +40,94 @@ export default function CreateUserModal() {
 
     },[])
 
+    function handleSubmit(e){
+        e.preventDefault()
+        sendUserCredentials()
+    }
 
+    async function sendUserCredentials(){
 
-    async function sendusercredentials(){
-        //TODO: validate input data here
+        //validate input data
+        if(!checkForErrors()){
+            console.log("correct errors first!")
+            return null
+        }
+
         const data = {
-            "username": username.current.value,
-            "email": email.current.value,
-            "password": password.current.value,
+            "username": formState.username,
+            "email": formState.email,
+            "password": formState.password,
             "invitedBy": invitedBy,
         }
+
         try {
             let res = await axios.post(API_URL +"accounts/", data).then((response) => {
             setInfoMessage(response.data["success-message"])
             });
 
         } catch (err) {
-            //TODO: check for verification
-
             const err_msg = err.response.data["error"]
             console.log(err_msg)
             setInfoMessage(err.response.data["error-message"])
         }
     }
+
+    const setField = (field, value) => {
+        console.log("setField()")
+        setFormState({
+            ...formState,
+            [field]: value
+        })
+        // remove old errors
+        setErrorsState({})
+
+    }
+
+    useEffect(()=>{
+        checkForErrors()
+    },[formState])
+
+
+    function checkForErrors(){
+        const newErrors = findFormErrors()
+
+        // Conditional logic:
+        if ( Object.keys(newErrors).length > 0 ) {
+
+            setErrorsState(newErrors)
+            console.log("INPUT BAD")
+            return false
+        } else {
+            console.log("INPUT FINE")
+            return true
+        }
+    }
+
+
+    const findFormErrors = () => {
+        console.log("findFormErrors()")
+        const { username,password, email } = formState
+        console.log("form:", formState)
+        const newErrors = {}
+
+        // name errors
+        if ( !username || username === '' ) newErrors.username = 'cannot be blank!'
+        else if ( username.length > 30 ) newErrors.username = 'username is too long!'
+
+        // email errors
+        if ( !email || email === '' ) newErrors.email = 'cannot be blank!'
+        else if ( email.length > 30 ) newErrors.email = 'email is too long!'
+
+        // password errors
+        if ( !password || password === '' ) newErrors.password = 'cannot be blank!'
+        else if ( password.length > 30 ) newErrors.password = 'password is too long!'
+
+
+
+        console.log("newErrors:", newErrors)
+        return newErrors
+    }
+
 
     return (
         <>
@@ -89,24 +153,39 @@ export default function CreateUserModal() {
                                 type="email"
                                 placeholder="name@example.com"
                                 autoFocus
-                                ref={email}
+                                onChange={ e => setField('email', e.target.value) }
+                                isInvalid={ !!errorsState.email }
                             />
+                            <Form.Control.Feedback type='invalid'>
+                                { errorsState.email }
+                            </Form.Control.Feedback>
                         </Form.Group>
+
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Username</Form.Label>
                             <Form.Control type="text"
                                           placeholder="username"
-                                          ref={username}
+                                          onChange={ e => setField('username', e.target.value) }
+                                          isInvalid={ !!errorsState.username }
                             />
+                            <Form.Control.Feedback type='invalid'>
+                                { errorsState.username }
+                            </Form.Control.Feedback>
                         </Form.Group>
+
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password"
                                           placeholder="***********"
-                                          ref={password}
+                                          onChange={ e => setField('password', e.target.value) }
+                                          isInvalid={ !!errorsState.password }
                             />
+                            <Form.Control.Feedback type='invalid'>
+                                { errorsState.password }
+                            </Form.Control.Feedback>
                         </Form.Group>
                     </Form>
+
                         {invitedBy && `You've been invited by ${invitedBy}!`}
                     <p>{infoMessage}</p>
                 </Modal.Body>
@@ -114,7 +193,7 @@ export default function CreateUserModal() {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={sendusercredentials} >Create Account</Button>
+                    <Button variant="primary" onClick={handleSubmit} >Create Account</Button>
                 </Modal.Footer>
             </Modal>
         </>
